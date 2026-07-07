210ac5fa991eb1cc5bb4f46243e955494a127977
moniishms <moniishms@gmail.com>
2026-07-07 15:34:34 +0530

    added roc

--- ml_model.py
+++ ml_model.py
@@ -1,8 +1,9 @@
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 from sklearn.tree import DecisionTreeClassifier
-from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, confusion_matrix
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, confusion_matrix,roc_auc_score,roc_curve
 
 # Load dataset generated earlier
 df = pd.read_csv("dataset.csv")
@@ -29,6 +30,7 @@ model.fit(X_train, y_train)
 
 # Predictions
 y_pred = model.predict(X_test)
+y_prob = model.predict_proba(X_test)
 
 # Metrics
 accuracy = accuracy_score(y_test, y_pred)
@@ -36,9 +38,34 @@ precision = precision_score(y_test, y_pred)
 recall = recall_score(y_test, y_pred)
 f1 = f1_score(y_test, y_pred)
 cm = confusion_matrix(y_test, y_pred)
+auc_score=roc_auc_score(y_test,y_prob[:,1])
 
 print("Accuracy:", accuracy)
 print("Precision:", precision)
 print("Recall:", recall)
 print("F1 Score:", f1)
 print("Confusion Matrix:\n", cm)
+print("ROC AUC Score:", auc_score)
+
+
+
+# ROC Curve
+fpr, tpr, thresholds = roc_curve(y_test, y_prob[:,1])
+
+# Plot ROC Curve
+plt.figure(figsize=(6,6))
+plt.plot(fpr, tpr, color='blue', linewidth=2,
+         label=f'ROC Curve (AUC = {auc_score:.3f})')
+
+# Random classifier
+plt.plot([0,1], [0,1], color='red', linestyle='--',
+         label='Random Classifier')
+
+# Labels
+plt.xlabel("False Positive Rate")
+plt.ylabel("True Positive Rate")
+plt.title("Receiver Operating Characteristic (ROC) Curve")
+plt.legend(loc="lower right")
+plt.grid(True)
+
+plt.show()
